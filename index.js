df3624ad5ca8c793d053b3190b6d2f958833c612
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-07-19 20:20:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function updateObjectWithKeyAndValue(object,key,value){
   object[key]=value
    return object
 }
+function destructivelyUpdaeObjectWithKeyAndValue(object,key,value){
+  object=Object.assign({},object[key]=value)
+  return object
+}
