f3eb11c74b7b0d354944a7bccc38648bd00ed8a7
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-07-19 20:28:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,7 @@ function deleteFromObjectByKey(object,key){
   delete newObj[key]
   return newObj;
 }
+function destructivelyDeleteFromObjectByKey(object,key){
+  delete object[key]
+  return object;
+}
